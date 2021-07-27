d0e555e9cf35febc3ca008191d271e8cf026719a
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-07-26 21:43:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const formularioElement = $("#formulario");   /* captura lo que ingresa el usuario, formulario completo */
 
-const inputElement = $("superhero-input");       /* capturar el valor del input */
+const inputElement = $("superhero-input");       /* capturar el valor del input en una variable*/
+
+const requestSection = document.getElementById("request-section");
 
 formularioElement.submit(function (event) {
     event.preventDefault();                     /* para q la pagina no se refresque */
 
 const idDelSuperHeroe = inputElement.val();
 
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: `https://superheroapi.com/api/1103444760144013/${idDelSuperHeroe}`,
+}).done(function(data) {
+    requestSection.innerHTML = JSON.strigyfy(data);
+});
 
 });
 
