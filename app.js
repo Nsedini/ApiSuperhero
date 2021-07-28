2d5f916445ed897b3b452e15179dbd519f149c27
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-07-28 19:17:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const formularioElement = $("#formulario");   /* captura lo que ingresa el usuario, formulario completo */
 
-const inputElement = $("superhero-input");       /* capturar el valor del input en una variable*/
+const inputElement = $("#superhero-input");       /* capturar el valor del input en una variable*/
 
 const requestSection = document.getElementById("request-section");
+
+function renderSuperHero(superhero) {
+    const imagen = document.getElementById("imagen")   /* js pelao   para ir a buscar al DOM */
+    const nombre = document.querySelector("#nombre")   /* js pelao  para ir a buscar al DOM*/
+
+    const publicadopor = $("#publicadopor");          /* jquery para ir a buscar al DOM*/
+    const ocupacion = $("#ocupacion")
+    const aparicion = $("#aparicion")
+    const altura = $("#altura")
+    const peso = $("#peso")
+    const alianzas = $("#alianzas")
+
+    imagen.setAttribute("src", superhero.image.url)            /* Para cambiar el source */
+}
 
 formularioElement.submit(function (event) {
     event.preventDefault();                     /* para q la pagina no se refresque */
@@ -12,10 +26,11 @@ const idDelSuperHeroe = inputElement.val();
 $.ajax({
     type: "GET",
     dataType: "json",
-    url: `https://superheroapi.com/api/1103444760144013/${idDelSuperHeroe}`,
+    url: `https://www.superheroapi.com/api.php/1103444760144013/${idDelSuperHeroe}`,
 }).done(function(data) {
-    requestSection.innerHTML = JSON.strigyfy(data);
+    console.log(data);
+    });
 });
 
-});
+
 
