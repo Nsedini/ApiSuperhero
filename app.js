0578a3c1f6b37fd00b6946e9fc2c521e6ec351d6
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-08-02 20:17:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,87 @@
-const formularioElement = $("#formulario");   /* captura lo que ingresa el usuario, formulario completo */
 
-const inputElement = $("#superhero-input");       /* capturar el valor del input en una variable*/
-
+const formularioElement = $("#formulario");     /* captura lo que ingresa el usuario, formulario completo */
+const inputElement = $("#superhero-input");     /* capturar el valor del input en una variable*/
 const requestSection = document.getElementById("request-section");
 
-function renderSuperHero(superhero) {
-    const imagen = document.getElementById("imagen")   /* js pelao   para ir a buscar al DOM */
-    const nombre = document.querySelector("#nombre")   /* js pelao  para ir a buscar al DOM*/
+console.log(formularioElement);
 
-    const publicadopor = $("#publicadopor");          /* jquery para ir a buscar al DOM*/
-    const ocupacion = $("#ocupacion")
-    const aparicion = $("#aparicion")
-    const altura = $("#altura")
-    const peso = $("#peso")
-    const alianzas = $("#alianzas")
+function renderSuperheroCard(superhero) {
+  const imagen = document.getElementById("imagen");    /* js pelao   para ir a buscar al DOM */
+  const nombre = document.querySelector("#nombre");    /* js pelao  para ir a buscar al DOM*/
 
-    imagen.setAttribute("src", superhero.image.url)            /* Para cambiar el source */
+  const publicadopor = $("#publicadopor");  /* jquery para ir a buscar al DOM*/
+  const ocupacion = $("#ocupacion");    /* jquery para ir a buscar al DOM*/
+  const aparicion = $("#aparicion");    /* jquery para ir a buscar al DOM*/
+  const altura = $("#altura");          /* jquery para ir a buscar al DOM*/ 
+  const peso = $("#peso");              /* jquery para ir a buscar al DOM*/
+  const alianzas = $("#alianzas");      /* jquery para ir a buscar al DOM*/
+
+  imagen.setAttribute("src", superhero.image.url);      /* Para cambiar el source */
+  nombre.innerHTML = `Nombre: ${superhero.name}`;
+
+  publicadopor.html(superhero.biography.publisher);
+  ocupacion.html(superhero.work.occupation);
+  aparicion.html(superhero.biography["first-appearance"]);
+  altura.html(
+    `${superhero.appearance.height[0]} - ${superhero.appearance.height[1]}`
+  );
+  peso.html(
+    `${superhero.appearance.weight[0]} - ${superhero.appearance.weight[1]}`
+  );
+  alianzas.html(superhero.biography.aliases.join(" - "));
 }
 
-formularioElement.submit(function (event) {
-    event.preventDefault();                     /* para q la pagina no se refresque */
+function renderSuperheroChart(superhero) {
+  const options = {
+    title: {
+      text: "Grafico del Superheroe",
+    },
+    data: [
+      {
+        type: "pie",
+        dataPoints: [
+          {
+            label: "Inteligencia",
+            y: Number.parseInt(superhero.powerstats.intelligence),
+          },
+          {
+            label: "Fuerza",
+            y: Number.parseInt(superhero.powerstats.strength),
+          },
+          {
+            label: "Velocidad",
+            y: Number.parseInt(superhero.powerstats.speed),
+          },
+          {
+            label: "Durabilidad",
+            y: Number.parseInt(superhero.powerstats.durability),
+          },
+          {
+            label: "Poder",
+            y: Number.parseInt(superhero.powerstats.power),
+          },
+          {
+            label: "Combate",
+            y: Number.parseInt(superhero.powerstats.combat),
+          },
+        ],
+      },
+    ],
+  };
+  $("#chartContainer").CanvasJSChart(options);
+}
 
-const idDelSuperHeroe = inputElement.val();
+formularioElement.submit(function (event) {         /* para q la pagina no se refresque */
+  event.preventDefault();
 
-$.ajax({
+  const idDelSuperHeroe = inputElement.val();
+
+  $.ajax({
     type: "GET",
     dataType: "json",
-    url: `https://www.superheroapi.com/api.php/1103444760144013/${idDelSuperHeroe}`,
-}).done(function(data) {
-    console.log(data);
-    });
+    url: `https://www.superheroapi.com/api.php/10226095432389769/${idDelSuperHeroe}`,
+  }).done(function (data) {
+    renderSuperheroCard(data);
+    renderSuperheroChart(data);
+  });
 });
-
-
-
